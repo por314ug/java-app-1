25edc7b7c9cb90358cf6679711eacd5a3d58e760
por314ug <162358126+por314ug@users.noreply.github.com>
2024-04-12 10:44:43 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ public class Main {
 
         System.out.println("Hello world!");
 
-        for (int i = 1; i <= 5; i++) {
+        for (int i = 1; i <= ; i++) {
             System.out.println("i = " + i);
         }
     }
